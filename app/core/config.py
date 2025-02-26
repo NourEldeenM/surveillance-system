13ee5747e9d47ac94db07005faa6b1669633471f
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2025-02-26 21:00:30 +0200

    feat(auth): add login & signup using JWT

--- app/core/config.py
+++ app/core/config.py
@@ -8,3 +8,16 @@ class DatabaseConfig:
 
 # Singleton instance of Config
 DATABASE = DatabaseConfig()
+
+class AuthConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    HASH_ALGORITHM = os.getenv("HASH_ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY is not set in environment variables or .env file!")
+    if not HASH_ALGORITHM:
+        raise ValueError("HASH_ALGORITHM is not set in environment variables or .env file!")
+    if not ACCESS_TOKEN_EXPIRE_MINUTES:
+        raise ValueError("ACCESS_TOKEN_EXPIRE_MINUTES is not set in environment variables or .env file!")
+    
