b1323385b02cef20dfa8ef16a5224414c4fd89a9
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2025-03-01 06:17:42 +0200

    feat(branch): add new module branch

---
+++ app/models/branch.py
@@ -0,0 +1,13 @@
+from typing import List
+from uuid import uuid4
+from sqlalchemy import Column, String
+from app.models.database import Base
+from app.models.user import User
+
+class Branch(Base):
+    __tablename__ = "branches"
+
+    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    address = Column(String, index=True)
+    postal_code = Column(String, index=True)
+    staff = List[User]
---
+++ app/routers/branch.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.models.database import SessionLocal
+from app.schemas.branch import BranchCreate, BranchResponse
+from app.services.branch import BranchService
+
+router = APIRouter()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("", response_model=BranchResponse)
+def create_branch_route(branch_data: BranchCreate, db: Session = Depends(get_db)):
+    return BranchService.create_branch(branch_data, db)
