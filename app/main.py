e9574ff45749c36eac888f58e7aeeb46387ec3bd
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2025-03-26 08:31:52 +0200

    remove backend folder temporarily till finishing the backend

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.routers.auth import router as auth_router
 from app.routers.branch import router as branch_router
 from app.routers.region import router as region_router
 from app.utils.register_exception_handlers import register_exception_handlers
+from app.routers.face import router as face_router
 
 app = FastAPI()
 
@@ -14,3 +15,4 @@ app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(auth_router, prefix="", tags=["Auth"])
 app.include_router(region_router, prefix="/regions", tags=["Regions"])
 app.include_router(branch_router, prefix="/branches", tags=["Branches"])
+app.include_router(face_router, prefix="/models/face", tags=["Models"])
